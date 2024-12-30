0022ec4ca62d707eb506c73e66654915329dd223
Debiddo618 <dzheng@terpmail.umd.edu>
2024-12-30 10:19:23 -0500

    Set up spring-boot to load database credentials directly from .env

--- src/main/java/com/example/employee_reimbursement_system/EmployeeReimbursementSystemApplication.java
+++ src/main/java/com/example/employee_reimbursement_system/EmployeeReimbursementSystemApplication.java
@@ -6,10 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class EmployeeReimbursementSystemApplication {
 
 	public static void main(String[] args) {
+		// Load the .env file
+		Dotenv dotenv = Dotenv.load();
+
+		// Set environment variables to system properties
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		
 		SpringApplication.run(EmployeeReimbursementSystemApplication.class, args);
 	}
 

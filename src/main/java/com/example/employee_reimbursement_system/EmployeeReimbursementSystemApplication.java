d26f0270b63809ec907ff40fe341bd7fa23cfc59
Debiddo618 <dzheng@terpmail.umd.edu>
2024-12-30 20:11:35 -0500

    comment out dotenv settings

--- src/main/java/com/example/employee_reimbursement_system/EmployeeReimbursementSystemApplication.java
+++ src/main/java/com/example/employee_reimbursement_system/EmployeeReimbursementSystemApplication.java
@@ -12,15 +12,15 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class EmployeeReimbursementSystemApplication {
 
 	public static void main(String[] args) {
-		// Load the .env file
-		Dotenv dotenv = Dotenv.load();
+	// 	// Load the .env file
+	// 	Dotenv dotenv = Dotenv.load();
 
-		// Set environment variables to system properties
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(),
-		entry.getValue()));
+	// 	// Set environment variables to system properties
+	// 	dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(),
+	// 	entry.getValue()));
 
-		SpringApplication.run(EmployeeReimbursementSystemApplication.class, args);
-	}
+	// 	SpringApplication.run(EmployeeReimbursementSystemApplication.class, args);
+	// }
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
